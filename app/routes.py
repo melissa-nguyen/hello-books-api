0a5c93735a5676564c49b48839d3b1f85ed23828
melissa-nguyen <n.melissa.25@gmail.com>
2021-05-04 12:06:40 -0700

    created book_id endpoint to get records of a singular book

--- app/routes.py
+++ app/routes.py
@@ -30,17 +30,37 @@ def broken_endpoint():
 
 from app import db
 from app.models.book import Book
-from flask import request, Blueprint, make_response
+from flask import request, Blueprint, make_response, jsonify
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"], description=request_body["description"])
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"], description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
-    
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
