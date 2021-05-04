ec5dbc6f08dc16719fe01f18b78f65dae9e4249a
melissa-nguyen <n.melissa.25@gmail.com>
2021-05-04 13:21:26 -0700

    added a put and delete method to book id endpoint

--- app/routes.py
+++ app/routes.py
@@ -55,12 +55,26 @@ def handle_books():
 
         return make_response(f"Book {new_book.title} successfully created", 201)
 
-@books_bp.route("/<book_id>", methods=["GET"])
+@books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"])
 def handle_book(book_id):
     book = Book.query.get(book_id)
+    if request.method == "GET":
+        return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+        book.title = form_data["title"]
+        book.description = form_data["description"]
+
+        db.session.commit()
+
+        return make_response(f"Book #{book.id} successfully updated")
+    elif request.method == "DELETE":
+        db.session.delete(book)
+        db.session.commit()
+        return make_response(f"Book #{book.id} successfully deleted")
+
