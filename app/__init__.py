835cacb4480b8c277f28a3a3b8ee732960e6a887
melissa-nguyen <n.melissa.25@gmail.com>
2021-05-10 10:15:49 -0700

    created env file

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,10 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(“SQLALCHEMY_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
-
 
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
